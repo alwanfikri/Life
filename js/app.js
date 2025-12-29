9191391a923d926e9027b9be8859fb1d7232984a
alwanfikri <66763569+alwanfikri@users.noreply.github.com>
2025-12-29 07:21:59 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
-import { openDB } from './db.js'
-
-console.log('APP MODULE LOADED')
-
-window.onload = async () => {
-  console.log('WINDOW ONLOAD')
-  await openDB()
-  console.log('DB OPEN ATTEMPTED')
-}
+throw new Error('APP.JS IS EXECUTING');
