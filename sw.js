cd861bb68c71f66300c8e832541a76865155612b
alwanfikri <66763569+alwanfikri@users.noreply.github.com>
2025-12-29 11:02:17 +0700

    edit

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'shared-life-v2';
+const CACHE_NAME = 'shared-life-v3';
+
+const CACHE_URLS = [
+  '/',
+  '/index.html',
+  '/css/base.css',
+  '/css/layout.css',
+  '/js/db.js',
+  '/js/users.js',
+  '/js/journal.js',
+  '/js/schedule.js',
+  '/js/finance.js',
+  '/manifest.json'
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/base.css',
-        '/css/layout.css',
-        '/js/app.js',
-        '/js/db.js',
-        '/js/users.js',
-        '/js/journal.js',
-        '/js/schedule.js',
-        '/js/finance.js',
-        '/manifest.json'
-      ]);
+      return cache.addAll(CACHE_URLS);
     })
   );
 });
@@ -24,6 +25,20 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
