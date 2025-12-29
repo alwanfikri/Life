515072c8665d798fc189438c16270d6b8aedfbd2
alwanfikri <66763569+alwanfikri@users.noreply.github.com>
2025-12-29 14:23:26 +0700

    Add files via upload

--- js/finance.js
+++ js/finance.js
@@ -1,15 +1,17 @@
 import { save } from './db.js'
 import { getUser } from './users.js'
 
-export async function addFinance(amount, type, shared=false) {
+export async function addFinance(amount, type, category, shared=false) {
   const item = {
     id: crypto.randomUUID(),
     owner: getUser(),
     shared,
     amount,
-    type, // "in" or "out"
+    type,
+    category,  // 🆕
     createdAt: Date.now(),
     updatedAt: Date.now()
   }
+
   await save('finance', item)
 }
--- js/schedule.js
+++ js/schedule.js
@@ -1,16 +1,18 @@
 import { save } from './db.js'
 import { getUser } from './users.js'
 
-export async function addSchedule(title, start, end, shared=false) {
+export async function addSchedule(title, start, end, category, shared=false) {
   const item = {
     id: crypto.randomUUID(),
     owner: getUser(),
     shared,
     title,
+    category,   // 🆕
     start,
     end,
     createdAt: Date.now(),
     updatedAt: Date.now()
   }
+
   await save('schedules', item)
 }
